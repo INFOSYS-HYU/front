794f22145b65f2f0d0ad9e4ff6cd32a6a96a3700
sysys10 <166797095+sysys10@users.noreply.github.com>
2024-08-13 16:03:44 +0900

    login

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ import Footer from "./components/ui/footer.tsx";
 import NoticeList from "./pages/Notice/NoticeList.tsx";
 import Notice from "./pages/Notice/Notice.tsx";
 import Faq from "./pages/Faq/Faq.tsx";
-import Login from "./pages/auth/Login.tsx";
-import Mypage from "./pages/auth/Mypage.tsx";
+import Login from "./pages/Auth/Login.tsx";
+import Mypage from "./pages/Auth/Mypage.tsx";
 
 function Layout() {
   return (
